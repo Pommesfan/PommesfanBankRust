55376abd886d25d9cb90d0b98d7b7e823a67c3d5
Johannes Wirbser <johannes.wirbser@gmx.de>
2025-06-06 20:16:18 +0200

    Initially pass a command over network connection

--- Client/src/main.rs
+++ Client/src/main.rs
@@ -1,3 +1,10 @@
+use std::{cmp, net::UdpSocket, io};
+
 fn main() {
-    println!("Hello, world!");
+	let buf = [4, 3, 2, 2, 1, 4, 3, 2, 2, 1];
+	let socket = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind to address");
+    println!("Kommando eingeben:");
+    let mut cmd = String::new();
+    io::stdin().read_line(&mut cmd);
+	socket.send_to(cmd.as_bytes(), "127.0.0.1:34254").expect("couldn't send data");
 }
--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::UdpSocket, str};
+
+fn main() -> std::io::Result<()> {
+    {
+        let socket = UdpSocket::bind("127.0.0.1:34254")?;
+
+        // Receives a single datagram message on the socket. If `buf` is too small to hold
+        // the message, it will be cut off.
+        println!("{:?}", "Waiting for input");
+        let mut buf = [0; 10];
+        let (amt, src) = socket.recv_from(&mut buf)?;
+        let res = String::from_utf8(buf.to_vec()).unwrap();
+        println!("{}", &res);
+    } // the socket is closed here
+    Ok(())
 }
