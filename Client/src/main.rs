a34fa44d5c9549297957baf784f0cc4c233df1c1
Johannes Wirbser <johannes.wirbser@gmx.de>
2025-06-25 16:31:51 +0200

    initially added encryption

--- Client/src/main.rs
+++ Client/src/main.rs
@@ -1,10 +1,22 @@
 use std::{cmp, net::UdpSocket, io};
+use aes::cipher::{block_padding::Pkcs7, BlockDecryptMut, BlockEncryptMut, KeyInit};
+use hex_literal::hex;
+
+type Aes128EcbEnc = ecb::Encryptor<aes::Aes128>;
 
 fn main() {
-	let buf = [4, 3, 2, 2, 1, 4, 3, 2, 2, 1];
 	let socket = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind to address");
     println!("Kommando eingeben:");
     let mut cmd = String::new();
     io::stdin().read_line(&mut cmd);
-	socket.send_to(cmd.as_bytes(), "127.0.0.1:34254").expect("couldn't send data");
+
+    let mut buf = [0u8; 48];
+    let pt_len = cmd.len();
+    buf[..pt_len].copy_from_slice(&(cmd.as_bytes()));
+
+    let key = [0x42; 16];
+    let ct = Aes128EcbEnc::new(&key.into()).encrypt_padded_mut::<Pkcs7>(&mut buf, pt_len)
+    .unwrap();
+
+	socket.send_to(ct, "127.0.0.1:34254").expect("couldn't send data");
 }
--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,4 +1,9 @@
+use aes::cipher::{block_padding::Pkcs7, BlockDecryptMut, BlockEncryptMut, KeyInit};
+use hex_literal::hex;
 use std::{net::UdpSocket, str};
+
+type Aes128EcbDec = ecb::Decryptor<aes::Aes128>;
+
 
 fn main() -> std::io::Result<()> {
     {
@@ -7,9 +12,14 @@ fn main() -> std::io::Result<()> {
         // Receives a single datagram message on the socket. If `buf` is too small to hold
         // the message, it will be cut off.
         println!("{:?}", "Waiting for input");
-        let mut buf = [0; 10];
+        let mut buf = [0; 32];
         let (amt, src) = socket.recv_from(&mut buf)?;
-        let res = String::from_utf8(buf.to_vec()).unwrap();
+
+        let key = [0x42; 16];
+        let mut buf2 = [0u8; 48];
+        let pt = Aes128EcbDec::new(&key.into()).decrypt_padded_b2b_mut::<Pkcs7>(&buf, &mut buf2).unwrap();
+
+        let res = String::from_utf8(pt.to_vec()).unwrap();
         println!("{}", &res);
     } // the socket is closed here
     Ok(())
