fdd8ca7de89af61015db6735bdb4629d20c4ff52
Johannes Wirbser <johannes.wirbser@gmx.de>
2025-09-28 12:12:00 +0200

    little correction in creating hashcode

--- common/src/utils.rs
+++ common/src/utils.rs
@@ -5,6 +5,7 @@ pub fn create_hashcode_sha256(s: &String) -> [u8; 32] {
     let b =  s.as_bytes();
     let mut hasher = Sha256::new();
     hasher.update(b);
+    let b = hasher.finalize();
     let mut b_owned: [u8; 32] = [0; 32];
     b_owned[..b.len()].copy_from_slice(&b);
     b_owned
